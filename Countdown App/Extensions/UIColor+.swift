55870ddde27ad96a4702833c5c1bf1ac6563542b
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-08-27 22:45:19 +0300

    created main app colors extension for UIColor

---
+++ Countdown App/Extensions/UIColor+.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+.swift
+//  Countdown App
+//
+//  Created by Алексей Павленко on 27.08.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let whiteColor = UIColor(named: "appWhite")!
+    static let blueColor = UIColor(named: "blue")!
+    static let lightBlueColor = UIColor(named: "lightBlue")!
+    static let darkBlueColor = UIColor(named: "darkBlue")!
+    static let superLightBlueColor = UIColor(named: "superLightBlue")
+    
+}
