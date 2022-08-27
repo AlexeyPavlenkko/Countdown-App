818b2ce7ab7bb4f5861449b1eeab4bfcae7d74e5
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-08-27 22:50:18 +0300

    renamed to "MainViewController"

--- Countdown App/Code/Modules/Main/MainViewController.swift
+++ Countdown App/Code/Modules/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Countdown App
 //
 //  Created by Алексей Павленко on 27.08.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
