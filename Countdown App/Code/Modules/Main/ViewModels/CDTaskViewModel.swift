4d07999af5e260980b22e9f10a241a1353b316e1
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-08-28 20:06:57 +0300

    added functionality in order to get/set hours, minutes, seconds + get if is task valid + func to compute total seconds

--- Countdown App/Code/Modules/Main/ViewModels/CDTaskViewModel.swift
+++ Countdown App/Code/Modules/Main/ViewModels/CDTaskViewModel.swift
@@ -28,7 +28,7 @@ class CDTaskViewModel {
     
     private var hours = Box(0)
     private var minutes = Box(0)
-    private var second = Box(0)
+    private var seconds = Box(0)
     
     //MARK: - Init
     
@@ -62,4 +62,61 @@ class CDTaskViewModel {
     func getTaskTypes() -> [CDTaskType] {
         self.taskTypes
     }
+    
+    func setHours(to value: Int) {
+        
+        self.hours.value = value
+    }
+    
+    func setMinutes(to value: Int) {
+        var newMinutes = value
+        
+        if value >= 60 {
+            newMinutes -= 60
+            hours.value += 1
+        }
+        
+        self.minutes.value = newMinutes
+    }
+    
+    func setSeconds(to value: Int) {
+        var newSeconds = value
+        
+        if value >= 60 {
+            newSeconds -= 60
+            minutes.value += 1
+        }
+        
+        if minutes.value >= 60 {
+            minutes.value -= 60
+            hours.value += 1
+        }
+        
+        self.seconds.value = newSeconds
+    }
+    
+    func getHours() -> Box<Int> {
+        return self.hours
+    }
+    
+    func getMinutes() -> Box<Int> {
+        return self.minutes
+    }
+    
+    func getSeconds() -> Box<Int> {
+        return self.seconds
+    }
+    
+    func computeSeconds() {
+        self.cdtask.second = (hours.value * 3600) + (minutes.value * 60) + seconds.value
+        self.cdtask.timeStamp = Date().timeIntervalSince1970
+    }
+    
+    func isTaskValid() -> Bool {
+        if !cdtask.name.isEmpty && !cdtask.description.isEmpty && selectedIndex != -1 && (seconds.value > 0 || minutes.value > 0 || hours.value > 0) {
+            return true
+        }
+        return false
+    }
+    
 }
